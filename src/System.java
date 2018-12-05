dd13d854fea773e4f9d249e32c50df3293163603
Evan Cann <sEvanCann@yccc.edu>
2018-12-05 18:05:26 -0500

    first commit

---
+++ src/System.java
@@ -0,0 +1,44 @@
+
+public class System {
+	
+	private static int e;
+	private static int c;
+	private static int d;
+	
+	
+	public int getE() {
+		return e;
+	}
+	public void setE(int e) {
+		this.e = e;
+	}
+	public int getC() {
+		return c;
+	}
+	public void setC(int c) {
+		this.c = c;
+	}
+	public int getD() {
+		return d;
+	}
+	public void setD(int d) {
+		this.d = d;
+	}
+	
+	
+public void integrate()
+{
+int exponent = e;
+int coefficient = c;
+int denominator = d;
+
+e++;
+c=c;
+d=(e+1)*d;
+
+}
+	
+
+	
+	
+}
